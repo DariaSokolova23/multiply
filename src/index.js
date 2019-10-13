755425fb9f41bf482419ed1d6c42845640f8b27f
Daria Sokolova <dasha-inet@mail.ru>
2019-10-13 15:31:10 +0400

    feat: inplementation of algoruthm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  var parsedFirst= parseInt(first,10);
+  var parsedSecond= parseInt(second,10);
+  var totalResult=parsedFirst*parsedSecond;
+  //создадим массив для разбивания больших чисел
+  var BigArr=[first]
+  return String(totalResult);
 }
